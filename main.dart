5f5f5386768f22bf6dad20aac46c698f53c34801
Umer Naeem Qureshi <57386070+umerqureshi94@users.noreply.github.com>
2022-10-07 23:41:01 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,3 +1,94 @@
-void main() {
-  print("Hello world");
-}
+//Q.1: Create two integer variables length and breadth and assign values then check if they are square values or rectangle values.
+//ie: if both values are equal then it's square otherwise rectangle.
+import 'dart:io';
+
+// void main() {
+//   int len = 9;
+//   int Bth = 8;
+
+//   if (len == Bth) {
+//     print("It is Square");
+//   } else {
+//     print("It is Rectangle");
+//   }
+// }
+
+//Q.5  Write a program to read temperature in centigrade and display a suitable message according to temperature
+// void main() {
+//   num temp = 42;
+//   if (temp < 0) {
+//     print("Freezing Weather");
+//   } else if (temp <= 10) {
+//     print("Very Cold Weather");
+//   } else if (temp <= 20) {
+//     print("Cold Weather");
+//   } else if (temp <= 30) {
+//     print("Normal Weather");
+//   } else if (temp < 40) {
+//     print("Hot Weather");
+//   } else if (temp >= 40) {
+//     print("Very Hot");
+//   }
+// }
+
+// Q.6: Write a program to check whether an alphabet is a vowel or consonant.
+// void main() {
+//   var alph ="X";
+// if (alph == 'a'||alph =='e'||alph =='i'||alph =='o' ||alph =='u'){
+//   print ("Vowel");
+  
+//   }
+// else if (alph == 'A'||alph =='E'||alph =='I'||alph =='O' ||alph =='U'){
+//   print ("Vowel");
+  
+//   }
+
+//   else{
+//     print("Consonant");
+//   }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// Q.7: Write a program to calculate and print the Electricity bill of a given customer.
+
+
+// void main() {
+//   print("Electricity Bill");
+//   int cust_id = 1001;
+//   String cust_Name = "James";
+//   var units = 800;
+//   var unit;
+//   num nba = 0.00;
+//   if (units <= 199) {
+//     unit = units * 1.20;
+//     print("@Rs. 1.20 per unit :$unit");
+//     nba = units * 1.20 + 240;
+//   } else if (units <= 400) {
+//     unit = units * 1.50;
+//     print("@Rs. 1.50 per unit :$unit");
+//     nba = units * 1.50 + 240;
+//   } else if (units <= 600) {
+//     unit = units * 1.80;
+//     print("@Rs. 1.80 per unit :$unit");
+//     nba = units * 1.80 + 240;
+//   } else if (units > 600) {
+//     unit = ("@Rs. 2.00 per unit :${units * 2.00}");
+//     nba = units * 2.00 + 240;
+//   }
+//   print("Customer ID.NO.: $cust_id");
+//   print("Customer Name: $cust_Name");
+//   print("Units Consumed: $units");
+//   print("Amount Charges: $unit");
+//   print("Net Bill Amount: $nba");
+// }
